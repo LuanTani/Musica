e33166a00db60b076cdd4fdb03f1578d43187a0e
unknown <luanpaivatani@gmail.com>
2020-09-11 22:31:11 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        //<---Inicio(Titulo & Cantor)--->//
+
+             Console.Write("Titulo: ");
+             Console.ForegroundColor = ConsoleColor.DarkYellow;
+             Console.Write ("Breaking The Habit");
+             Console.Write("\n");
+             Console.ResetColor();
+
+             Console.Write("Cantor: ");
+             Console.ForegroundColor = ConsoleColor.DarkBlue;
+             Console.WriteLine("Link Park");
+             Console.Write("\n");
+             Console.ResetColor();
+             //<---Fim(Titulo & Cantor)--->//
+
+             Console.WriteLine("Memories consume");
+             Console.WriteLine("Like opening the wound");
+             Console.WriteLine("I'm picking me apart again");
+             Console.WriteLine("You all assume");
+             Console.WriteLine("I'm safe here in my room");
+             Console.WriteLine("Unless I try to start again");
+             Console.Write("\n");
+
+              Console.WriteLine("I don't want to be the one");
+              Console.WriteLine("Who the battles always choose");
+              Console.WriteLine("Cause inside I realize");
+              Console.WriteLine("That I'm the one confused");
+              Console.Write("\n");
+ 
+              //<---Inicio(Refão 1)--->//
+              Console.ForegroundColor = ConsoleColor.DarkRed;  
+              Console.WriteLine("I don't know what's worth fighting for");
+              Console.WriteLine("Or why I have to scream");
+              Console.WriteLine("I don't know why I instigate");
+              Console.WriteLine("And say what I don't mean");
+              Console.Write("\n");
+
+              Console.WriteLine("I don't know how I got this way");
+              Console.WriteLine("I know it's not alright");
+              Console.WriteLine("So I'm breaking the habit");
+              Console.WriteLine("I'm breaking the habit");
+              Console.WriteLine("Tonight");
+              Console.ResetColor();
+              Console.Write("\n");
+              //<---Fim(Refrão 1)--->//
+
+              //<---Inicio(Refão 2)--->//
+              Console.ForegroundColor = ConsoleColor.DarkGreen;  
+              Console.WriteLine("Clutching my cure");
+              Console.WriteLine("I tightly lock the door");
+              Console.WriteLine("I try to catch my breath again");
+              Console.WriteLine("I hurt much more");
+              Console.WriteLine("Than anytime before");
+              Console.WriteLine("I had no options left again");
+              Console.Write("\n");
+                 
+              Console.WriteLine("I don't want to be the one");
+              Console.WriteLine("Who the battles always choose");
+              Console.WriteLine("Cause inside I realize");
+              Console.WriteLine("That I'm the one confused");
+              Console.ResetColor();
+              Console.Write("\n");
+              //<---Fim(Refrão 2)--->//
+
+              //<---Inicio(Refrão 3)--->//
+              Console.ForegroundColor = ConsoleColor.DarkMagenta;
+              Console.WriteLine("I don't know what's worth fighting for");
+              Console.WriteLine("Or why I have to scream");
+              Console.WriteLine("I don't know why I instigate");
+              Console.WriteLine("And say what I don't mean");
+              Console.Write("\n");
+                 
+              Console.WriteLine("I don't know how I got this way");
+              Console.WriteLine("I'll never be alright");
+              Console.WriteLine("So I'm breaking the habit");
+              Console.WriteLine("I'm breaking the habit");
+              Console.WriteLine("Tonight");
+              Console.Write("\n");
+
+              Console.WriteLine("I'll paint it on the walls");
+              Console.WriteLine("Cause I'm the one at fault");
+              Console.WriteLine("I'll never fight again");
+              Console.WriteLine("And this is how it ends");
+              Console.Write("\n");
+
+              Console.WriteLine("I don't know what's worth fighting for");
+              Console.WriteLine("Or why I have to scream");
+              Console.WriteLine("But now I have some clarity");
+              Console.WriteLine("To show you what I mean");
+              Console.Write("\n");
+
+              Console.WriteLine("I don't know how I got this way");
+              Console.WriteLine("I'll never be alright");
+              Console.WriteLine("So I'm breaking the habit");
+              Console.WriteLine("I'm breaking the habit");
+              Console.WriteLine("I'm breaking the habit");
+              Console.WriteLine("Tonight");
+              Console.Write("\n");
+              Console.ResetColor();
+              //<---Fim(Refrão 3)--->//
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Faça um programa que exiba uma letra de música
 **Enunciado**:
 
 Faça um programa que exiba uma letra de música a sua escolha. Utilize cores em destaque no título e nos refrões.
+
+**Como Ececutar**
+
+Acesse a pasta do projeto e digite:
+```
+dotnet run
+```
